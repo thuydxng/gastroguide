ff9bef00849f41977bd5e5e67aec98a128cfeb9b
Hoang Nguyen <hoang.nguyen@xing.com>
2017-07-17 14:11:40 +0200

    Change

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
-<!DOCTYPE html> 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="author" content="Mein Name" />	
-	<!-- Das neueste kompilierte und minimierte CSS -->
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-   <link rel="StyleSheet" href="css/cssstyles.css" type="text/css" media="screen">
-   <style>pre{width: 600px}</style>	
-   <title>Gastroguide</title>     
-</head>
-<body>
-	<?php
-		
-		include('tools/connect.php');
-		include('tools/DBTools.php');
-		$datenbank = new DBTools($dbm);
-		if ($_GET){
-			echo '<div class="container">';
-			include('includes/header.php');
-			include('includes/navbar.php');
-			if ($_GET['x'] == "start") 
-			{
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="author" content="Mein Name" />
+		<!-- Das neueste kompilierte und minimierte CSS -->
+	   <link rel="StyleSheet" href="css/main.css" type="text/css" media="screen">
+	   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	   <style>pre{width: 600px}</style>
+	   <title>Gastroguide</title>
+	</head>
+	<body>
+		<?php
+			include('tools/connect.php');
+			include('tools/DBTools.php');
+			$datenbank = new DBTools($dbm);
+			if ($_GET) {
+				echo '<div class="container">';
+				include('includes/header.php');
+				include('includes/navbar.php');
+
+				if ($_GET['x'] == "start") {
+					include ('pages/start.php');
+				}
+				include('includes/footer.php');
+				echo "</div>";
+			} else {
 				include ('pages/start.php');
-			}	
+			}
 
-			include('includes/footer.php');
-	        echo "</div>";
-		}
-		else {include ('pages/start.php');}
-
-	$dbm->close();
-	?>
-</body>
+		$dbm->close();
+		?>
+	</body>
 </html>
--- pages/start.php
+++ pages/start.php
@@ -1,11 +1,11 @@
-
-<div>
-<form class="navbar-form" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Suchen">
-        </div>
-        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-      </form>
+<div class="container text-center">
+  <section class="searchbar-section">
+    <form class="navbar-form" role="search">
+      <div class="form-group">
+        <input type="text" class="searchbar form-control" placeholder="Suchen">
+      </div>
+      <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+    </form>
+    <a href="/includes/navbar.php">weiter</a>
+  </section>
 </div>
-
-
